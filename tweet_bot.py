cb6456584925989832cf32eb17e8e713f0ad1703
Gabby Ortman <ortmangm@iastate.edu>
2016-03-15 19:32:17 -0500

    add tweet_graph.json, construct "markov chain"

--- tweet_bot.py
+++ tweet_bot.py
@@ -1,5 +1,6 @@
 import twitter
 import json
+import random
 from keys import *
 
 # Authenticate via OAuth
@@ -27,24 +28,65 @@ def getAllTweets(screen_name):
     for i in out:
         f.write("%s\n" % i)
 
-#who knows if this is a good way to do this. I sure don't know. 
+#who knows if this is a good way to do this. I sure don't know.
 def makeNodes():
     node_dict = {}
     f = open('my_tweets.txt', 'r')
     for lines in f:
         for word in lines.split(' '):
-            if 'http' not in word and '@' not in word and word not in tweet_graph:
-                node_dict[word] = []
+            node_dict[word.lower()] = []
     return node_dict
 
-def createEdges(nodes):
+def createEdges(graph):
     f = open('my_tweets.txt', 'r')
+    current_word = ''
+    next_word = ''
     for lines in f:
-        for word in lines.split('')
-            pass
+        line = lines.split()
+        print(line)
+        for i in range(len(line)):
+            current_word = line[i]
+            if(i < len(line) - 1):
+                next_word = line[i + 1]
+                graph[current_word.lower()].append(next_word.lower())
 
+    return graph
+
+def getGraph(filename):
+    f = open(filename, 'r')
+    graph = json.loads(f.read())
+    return graph
+
+def pickBeginning(graph):
+    beginning = []
+    while len(beginning) < 1:
+        beginning = graph[random.choice(graph.keys())]
+    return beginning[0]
+
+def constructSentence(previousState, graph):
+    hasNextState = True
+    sentence = previousState
+    current_word = ''
+    while hasNextState:
+        if graph.has_key(previousState):
+            current_word = random.choice(graph[previousState])
+            sentence += " " + current_word
+            previousState = current_word
+            hasNextState = True
+        else:
+            hasNextState = False
+    return sentence
 
 if __name__ == '__main__':
     #getAllTweets("nucle0tides")
-    graph = makeTweetGraph()
-    print(graph)
+    # graph = makeNodes()
+    # print(graph)
+    # graph = createEdges(graph)
+    # print(graph)
+    # json = json.dumps(graph)
+    # graph_json = open('tweet_graph.json', 'w')
+    # graph_json.write(json)
+    graph = getGraph('tweet_graph.json')
+    beginning = pickBeginning(graph)
+    sentence = constructSentence(beginning, graph)
+    print(sentence.encode('utf-8'))
